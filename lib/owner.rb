6556e0de10da879ab5be5a98c81dd82e654ed281
Ray Valdez <ray_valdez@hotmail.co.uk>
2018-10-21 14:29:08 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,38 +1,79 @@
 require 'pry'
 
 class Owner
-  attr_accessor :name
-  attr_reader :species
-  @@all = []
+  
+    attr_reader :species
+    attr_accessor :name, :pets
+    @@all = []
 
-  def initialize(species)
-    @species = species
+    def initialize(name, pets={fishes: [], cats: [], dogs: []}, species="human")
+      @name = name
+      @pets = pets
+      @species = species
+      @@all << self
+    end
 
-    @@all << self
-  end
+    def self.all
+      @@all
+    end
 
-  def self.all
-    @@all
-  end
+    def self.count
+      @@all.length
+    end
 
-  def self.reset_all
-    @@all.clear
-  end
+    def self.reset_all
+      @@all = []
+    end
 
-  def self.count
-    @@all.count
-  end
+    def say_species
+      "I am a #{@species}."
+    end
 
-  def say_species
-    "I am a #{species}."
-  end
+    def buy_fish(name)
+      fish = Fish.new(name)
+      @pets[:fishes] << fish
+    end
 
-  def pets
-    pets = {:cats=>[], :dogs=>[], :fishes =>[]}
-  end
+    def buy_cat(name)
+      cat = Cat.new(name)
+      @pets[:cats] << cat
+    end
 
-  def buy_fish(name)
-    name = Fish.new
+    def buy_dog(name)
+      dog = Dog.new(name)
+      @pets[:dogs] << dog
+    end
 
-  end
+    def walk_dogs
+      @pets[:dogs].each do|dog|
+        dog.mood = "happy"
+      end
+    end
+
+    def play_with_cats
+      @pets[:cats].each do|cat|
+        cat.mood = "happy"
+      end
+    end
+
+    def feed_fish
+      @pets[:fishes].each do|fish|
+        fish.mood = "happy"
+      end
+    end
+
+    def sell_pets
+      @pets.each do|key, value|
+        value.each do|pet|
+          pet.mood = "nervous"
+        end
+      end
+      @pets[:cats] = []
+      @pets[:dogs] = []
+      @pets[:fishes] = []
+    end
+
+    def list_pets
+      "I have #{@pets[:fishes].length} fish, #{@pets[:dogs].length} dog(s), and #{@pets[:cats].length} cat(s)."
+    end
 end

923c258bc3b28d32b43113d0742d6e50c811d8b5
Ray Valdez <ray_valdez@hotmail.co.uk>
2018-10-21 13:49:33 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,40 @@
+require 'pry'
+
 class Owner
-  # code goes here
+  attr_accessor :name
+  attr_reader :species
+  @@all = []
+
+  def initialize(species)
+    @species = species
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+
+  def self.count
+    @@all.count
+  end
+
+  def say_species
+    "I am a #{species}."
+  end
+
+  def pets
+    pets = {:cats=>[], :dogs=>[], :fishes =>[]}
+  end
+
+  def buy_fish(fish)
+    self.pets.each do |pet, count|
+      if pet == :fishes
+        binding.pry
+      end
+    end
+  end
 end
